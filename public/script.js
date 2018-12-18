a46cca92533138ad3c7af6601837203f2a2e1e10
Silva Kirsimae <silva.kirsimae-cic.netherlands@ibm.com>
2018-12-18 15:00:48 +0100

    Added functionality for incrementing and decrementing the price

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+var PRICE = 9.99;
+
 new Vue({
     el: '#app',
     data: {
@@ -11,22 +13,45 @@ new Vue({
     },
     methods: {
         addItem: function(index) {
-            this.total += 9.99;
+            this.total += PRICE;
             var item = this.items[index];
             var found = false;
             for (var i = 0; i < this.cart.length; i++) {
                 if (this.cart[i].id === item.id) {
                     found = true;
                     this.cart[i].qty++;
+                    break;
                 }
             }
             if (!found) {
                 this.cart.push({
                     id: item.id,
                     title: item.title,
-                    qty: 1
+                    qty: 1,
+                    price: PRICE
                 });
             }
+        },
+        inc: function(item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec: function(item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2));
         }
     }
 });
